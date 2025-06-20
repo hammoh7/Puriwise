9b59d5be24cc15e4da927a06517f8a8213d9ec45
hammoh7 <soahammohaadkar@gmail.com>
2025-06-20 18:19:50 +0530

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { RouteProvider } from "@/context/RouteContext";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${playfair.variable} bg-primary text-text`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <RouteProvider>{children}</RouteProvider>
+        </AuthProvider>
       </body>
     </html>
   );
